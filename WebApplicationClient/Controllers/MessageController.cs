e09a9860232158d82a9a7edd4f9f7b8e61fd90a0
Andriy Markus <andr98m@gmail.com>
2018-08-08 20:51:59 +0300

    message and theme controllers added

---
+++ WebApplicationClient/Controllers/MessageController.cs
@@ -0,0 +1,33 @@
+﻿using BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplicationClient.Controllers
+{
+    [RoutePrefix("api/Message")]
+    public class MessageController : ApiController
+    {
+        private readonly IMessageService messageService;
+        private readonly IThemeService themeService;
+
+        public MessageController(IMessageService messageService, IThemeService themeService)
+        {
+            this.messageService = messageService;
+            this.themeService = themeService;
+        }
+
+
+        //GET api/Message/GetMessagesByThemeId/50
+        [Route("GetMessagesByThemeId/{themeId:int}")]
+        public IHttpActionResult GetMessages(int themeId)
+        {
+            return Ok(new { theme = themeId, messages = this.messageService.GetByThemeId(themeId) });
+        }
+
+
+    }
+}
---
+++ WebApplicationClient/Controllers/ThemeController.cs
@@ -0,0 +1,39 @@
+﻿using BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplicationClient.Controllers
+{
+    [RoutePrefix("api/Theme")]
+    public class ThemeController : ApiController
+    {
+        private readonly IMessageService messageService;
+        private readonly IThemeService themeService;
+
+        public ThemeController(IMessageService messageService, IThemeService themeService)
+        {
+            this.messageService = messageService;
+            this.themeService = themeService;
+        }
+
+
+        //GET api/Theme/GetTheme/3
+        [Route("GetTheme/{themeId:int}")]
+        public IHttpActionResult GetTheme(int themeId)
+        {
+            return Ok(new { theme = this.themeService.GetById(themeId) });
+        }
+
+
+        //GET api/Theme/GetThemes
+        [Route("GetThemes")]
+        public IHttpActionResult GetThemes()
+        {
+            return Ok(new { themes = this.themeService.GetAll() });
+        }
+    }
+}
